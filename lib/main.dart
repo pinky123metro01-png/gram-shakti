c747f1dace8f6b9de7df3e9fa45da918c6f8a493
gram-shakti-official <pinky123metro01@gmail.com>
2026-03-01 10:15:09 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Center(
           child: Text(
             "Gram Shakti App Taiyar Hai!",
-            style: TextStyle(fontSize: 24),
+            style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
           ),
         ),
       ),
